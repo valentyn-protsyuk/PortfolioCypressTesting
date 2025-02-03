14572de4276fd172cdd36f0abc728a83de8b986c
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-03 14:08:54 -0800

    header element order test

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -6,6 +6,21 @@ describe("Header testing", () => {
     cy.reload();
   });
 
+  it("Header elements order", () => {
+    // 1 profile btn
+    cy.get('.header__menu li:nth-child(1)').contains('Profile');
+    // 2 links btn
+    cy.get('.header__menu li:nth-child(2)').contains('Links');
+    // 3 skills btn
+    cy.get('.header__menu li:nth-child(3)').contains('Skills');
+    // 4 projects btn
+    cy.get('.header__menu li:nth-child(4)').contains('Projects');
+    // 5 line
+    cy.get('.header__menu li:nth-child(5)').should('have.class','header__line');
+    // 6 sun btn
+    cy.get('.header__menu li:nth-child(6)').find('button.header__sun').should('exist');
+  });
+
   it("Profile btn hover color change", () => {
     // profile btn selector
     let profileSelector = '.header__link[href*="profile"]';
