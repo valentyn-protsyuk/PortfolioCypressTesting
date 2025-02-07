fd41b2ec29e3de9473859ed60f3d3a8db287eef5
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-07 13:58:27 -0800

    verify resume btn text underlined

--- cypress/e2e/linksDM.cy.ts
+++ cypress/e2e/linksDM.cy.ts
@@ -133,5 +133,9 @@ describe('LINKS SECTION TESTING in DARK MODE', () => {
         it('Verify btn initial color', () => {
             cy.get(LinksPage.resumeLink).should('have.css', 'color', 'rgba(225, 29, 79, 0.7)');
         });
+        
+        it('Verify btn text underlined', () => {
+            cy.get(LinksPage.resumeLink).should('have.css', 'text-decoration').and('include','underline');
+        });
     });
 });
