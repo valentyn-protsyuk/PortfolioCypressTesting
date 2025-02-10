eafd27e602307d07d45e3cbf294e02a0b7b43202
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-09 19:52:22 -0800

    hard skills tests for labels

--- cypress/e2e/skillsDM.cy.ts
+++ cypress/e2e/skillsDM.cy.ts
@@ -7,7 +7,7 @@ describe("SKILLS SECTION TESTING in DARK MODE", () => {
     cy.reload();
   });
 
-  describe("MY SKILLS CONTAINER Testing", () => {
+  describe("SKILLS CONTAINER Testing", () => {
     it("Verify skills container has vertical layout", () => {
       cy.get(SkillsPage.skillsContainer).should("have.css", "display", "flex");
       cy.get(SkillsPage.skillsContainer).should(
@@ -18,7 +18,7 @@ describe("SKILLS SECTION TESTING in DARK MODE", () => {
     });
   });
 
-  describe("MY LINKS MAIN TITLE TESTING", () => {
+  describe("LINKS MAIN TITLE TESTING", () => {
     it("Verify main title text", () => {
       cy.get(SkillsPage.mainTitle)
         .invoke("text")
@@ -42,7 +42,8 @@ describe("SKILLS SECTION TESTING in DARK MODE", () => {
     });
   });
 
-  describe('hard skills SUBTITLE TESTING', () => {
+
+  describe('HARD SKILLS SUBTITLE TESTING', () => {
           it('Verify subtitle text', () => {
               cy.get(SkillsPage.subTitles).eq(0).should('have.text', 'Hard Skills');
           });
@@ -51,5 +52,25 @@ describe("SKILLS SECTION TESTING in DARK MODE", () => {
               cy.get(SkillsPage.subTitles).eq(0).should('have.css', 'color', 'rgb(148, 163, 184)');
           });
     });
+
+    describe('HARD SKILLS LABELS TESTING', () => {
+        it('Verify 1st label text', () => {
+            cy.get(SkillsPage.hsLabels).eq(0).should('have.text', 'Testing');
+        });
+
+        it('Verify 2nd label text', () => {
+            cy.get(SkillsPage.hsLabels).eq(1).should('have.text', 'Programming');
+        });
+
+        it('Verify 3rd label text', () => {
+            cy.get(SkillsPage.hsLabels).eq(2).should('have.text', 'Other');
+        });
+
+        it('Verify text color for all labels', () => {
+            cy.get(SkillsPage.hsLabels).each(($el) => {
+                cy.wrap($el).should('have.css', 'color', 'rgb(241, 245, 249)');
+            });
+        });
+  });
 
 });
