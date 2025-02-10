6309c700e046be3bae5590af29fcbc64b51e1934
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-09 19:12:48 -0800

    skills main title tests

--- cypress/e2e/skillsDM.cy.ts
+++ cypress/e2e/skillsDM.cy.ts
@@ -1,17 +1,44 @@
 import { SkillsPage } from "../pages/Skills";
 import "cypress-real-events/support";
 
-describe('SKILLS SECTION TESTING in DARK MODE', () => {
+describe("SKILLS SECTION TESTING in DARK MODE", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.reload();
+  });
 
-    beforeEach(() => {
-        cy.visit('/');
-        cy.reload();
+  describe("MY SKILLS CONTAINER Testing", () => {
+    it("Verify skills container has vertical layout", () => {
+      cy.get(SkillsPage.skillsContainer).should("have.css", "display", "flex");
+      cy.get(SkillsPage.skillsContainer).should(
+        "have.css",
+        "flex-direction",
+        "column"
+      );
+    });
+  });
+
+  describe("MY LINKS MAIN TITLE TESTING", () => {
+    it("Verify main title text", () => {
+      cy.get(SkillsPage.mainTitle)
+        .invoke("text")
+        .should("match", /skills/i);
     });
 
-    describe('MY SKILLS CONTAINER Testing', () => {
-            it('Verify skills container has vertical layout', () => {
-                cy.get(SkillsPage.skillsContainer).should('have.css','display', 'flex');
-                cy.get(SkillsPage.skillsContainer).should('have.css','flex-direction', 'column');
-            });
+    it("Verify main title text is uppercase", () => {
+      cy.get(SkillsPage.mainTitle).should(
+        "have.css",
+        "text-transform",
+        "uppercase"
+      );
     });
+
+    it("Verify main title text color", () => {
+      cy.get(SkillsPage.mainTitle).should(
+        "have.css",
+        "color",
+        "rgb(241, 245, 249)"
+      );
+    });
+  });
 });
