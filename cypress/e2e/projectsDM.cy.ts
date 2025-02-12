4935e6ab68b9e57350808ea48352db8bef46e356
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-11 22:24:20 -0800

    project section tests for general styles

--- cypress/e2e/projectsDM.cy.ts
+++ cypress/e2e/projectsDM.cy.ts
@@ -7,6 +7,7 @@ describe("PROJECT SECTION TESTING in DARK MODE", () => {
     cy.reload();
   });
 
+  //SECTION TESTS
   describe("PROJECT CONTAINER Testing", () => {
     it("Verify section container has vertical layout", () => {
       cy.get(ProjectsPage.sectionContainer).should("have.css", "display", "flex");
@@ -28,7 +29,45 @@ describe("PROJECT SECTION TESTING in DARK MODE", () => {
         cy.get(ProjectsPage.sectionTitle).should('have.css', 'color', 'rgb(241, 245, 249)');
     });
 });
+//GENERAL STYLE TESTS
+describe.only("GENERAL STYLES TESTING", () => {
+    it("Verify project title text color", () => {
+      cy.get(ProjectsPage.projectTitles).each(($el) => {
+        cy.wrap($el).should("have.css", "color", "rgb(148, 163, 184)");
+        });
+    });
 
+    it("Verify project labels text color", () => {
+        cy.get(ProjectsPage.projectLabels).each(($el) => {
+          cy.wrap($el).should("have.css", "color", "rgb(241, 245, 249)");
+          });
+      });
+
+      it("Verify project Description text text color", () => {
+        cy.get(ProjectsPage.projectDescriptions).each(($el) => {
+          cy.wrap($el).should("have.css", "color", "rgb(71, 85, 105)");
+          });
+      });
+
+      it("Verify project list item text color", () => {
+        cy.get(ProjectsPage.projectTechItems).each(($el) => {
+          cy.wrap($el).should("have.css", "color", "rgb(71, 85, 105)");
+          });
+      });
+
+      it("Verify project links text color", () => {
+        cy.get(ProjectsPage.projectContentLinks).each(($el) => {
+          cy.wrap($el).should("have.css", "color", "rgba(225, 29, 79, 0.7)");
+          });
+      });
+
+      it("Verify project iframe has red border", () => {
+        cy.get(ProjectsPage.projectIframes).each(($el) => {
+          cy.wrap($el).should("have.css", "border-color", "rgba(225, 29, 79, 0.7)");
+          });
+      });
+});
+//PORTFOLIO PROJECT TESTS
       describe("PORTFOLIO PROJECT SUBTITLE TESTING", () => {
         it("Verify subtitle text", () => {
           cy.get(ProjectsPage.portfolioTitle).should("have.text", "Portfolio Website Testing");
