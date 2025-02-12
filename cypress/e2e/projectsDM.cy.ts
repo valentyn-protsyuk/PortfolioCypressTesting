61c068ab3c0d73aae8addfd953d8a0bbdca9af11
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-02-11 22:06:16 -0800

    portfolio project description label testing

--- cypress/e2e/projectsDM.cy.ts
+++ cypress/e2e/projectsDM.cy.ts
@@ -15,41 +15,39 @@ describe("PROJECT SECTION TESTING in DARK MODE", () => {
   });
 
   
-  describe('PROJECTS MAIN TITLE TESTING', () => {
-    it('Verify main title text', () => {
+  describe('PROJECTS SECTION TITLE TESTING', () => {
+    it('Verify title text', () => {
         cy.get(ProjectsPage.sectionTitle).invoke('text').should('match', /my projects/i);
     });
 
-    it('Verify main title text is uppercase', () => {
+    it('Verify title text is uppercase', () => {
         cy.get(ProjectsPage.sectionTitle).should('have.css', 'text-transform', 'uppercase');
     });
 
-    it('Verify main title text color', () => {
+    it('Verify title text color', () => {
         cy.get(ProjectsPage.sectionTitle).should('have.css', 'color', 'rgb(241, 245, 249)');
     });
 });
 
       describe("PORTFOLIO PROJECT SUBTITLE TESTING", () => {
         it("Verify subtitle text", () => {
-          cy.get(ProjectsPage.projectTitles).eq(0).should("have.text", "Portfolio Website Testing");
+          cy.get(ProjectsPage.portfolioTitle).should("have.text", "Portfolio Website Testing");
         });
     
         it("Verify subtitle text color", () => {
-          cy.get(ProjectsPage.projectTitles)
-            .eq(0)
+          cy.get(ProjectsPage.portfolioTitle)
             .should("have.css", "color", "rgb(148, 163, 184)");
         });
       });
 
       describe("PORTFOLIO PROJECT DESCRIPTION TESTING", () => {
         it("Verify Description title text", () => {
-          cy.get(ProjectsPage.projectTitles).eq(0).should("have.text", "Description");
+          cy.get(ProjectsPage.portfolioDescLabel).should("have.text", "Description");
         });
     
         it("Verify Description title text color", () => {
-          cy.get(ProjectsPage.projectTitles)
-            .eq(0)
-            .should("have.css", "color", "rgb(148, 163, 184)");
+          cy.get(ProjectsPage.portfolioDescLabel)
+            .should("have.css", "color", "rgb(241, 245, 249)");
         });
       });
       
